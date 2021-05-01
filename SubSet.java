fb60cc2506c1630693c3befac9810b5d58538db7
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-05-01 15:02:04 +0530

    array ques in java

--- SubSet.java
+++ SubSet.java
@@ -1,3 +1,15 @@
+/* PROGRAM TO PRINT ALL SUBSETS OF AN ARRAY*/
+/*ARRAY = 10 20 30 
+  OUTPUT: 
+ -    -   -
+ -    -   30
+ -    20  -
+ -    20  30
+ 10   -   -
+ 10   -   30
+ 10   20  -
+ 10   20  30    */
+
 import java.io.*;
 import java.util.*;
 
@@ -5,13 +17,16 @@ public class SubSet{
 
 public static void main(String[] args) {
     Scanner scn=new Scanner(System.in);
+    System.out.print("\n enter size of array : ");
     int n=scn.nextInt();
     int a[]=new int[n];
+    System.out.print("\n enter elements in array : ");
     for(int i=0;i<n;i++)
     {
         a[i]=scn.nextInt();
     }
     int l=(int)Math.pow(2,n);
+    System.out.print("\n output : \n");
     for(int i=0;i<l;i++)
     {
        int temp=i;
